1d71aeecd1e945a06cfe30704179c40434f9f830
fan-of-stl <abhishek1168953@gmail.com>
2023-11-08 02:18:42 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskAddComponent } from './components/task-add/task-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    TasksComponent,
+    TaskAddComponent
   ],
   imports: [
     BrowserModule,
